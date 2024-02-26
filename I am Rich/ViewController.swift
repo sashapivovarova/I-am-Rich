8f2dccf8ff661a0575acc2f77f318a2da6376c46
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2024-02-26 21:34:23 +0900

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Anastasia Grey on 2024/02/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
